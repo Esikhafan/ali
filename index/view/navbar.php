5852cfe55914c04ded60e481f222e30731027192
Esikhafan <167687314+Esikhafan@users.noreply.github.com>
2024-04-21 17:16:53 +0330

    Add files via upload

---
+++ index/view/navbar.php
@@ -0,0 +1,18 @@
+<div class="navbar">
+    <div class="container">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="view/blog/blog.php">بلاگ لوازم برقی و خانگی</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">درباره ما</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="view/blog/new-blog.php">وبلاگ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">تماس با ما</a>
+            </li>
+        </ul>
+    </div>
+</div> 
